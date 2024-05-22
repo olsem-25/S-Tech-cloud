e000eb6d6a94f4d0f31b71a0afb71157babe6db9
olsem-25 <64503069+olsem-25@users.noreply.github.com>
2024-05-22 14:07:13 +0000

    Disable on message forwarding

--- nodes/alice-toggle.js
+++ nodes/alice-toggle.js
@@ -103,9 +103,9 @@ module.exports = function(RED) {
 				return;
 			};
 			currentState.state.value = msg.payload;
-			node.send({
-				payload: currentState.state.value
-			});
+			// node.send({
+			// 	payload: currentState.state.value
+			// });
 			device.UpdateState(currentState);
 			node.status({fill:"green",shape:"dot", text:currentState.state.value.toString()});
 			if (done) {done();} 
